493505a3254d207d27168c9aec6e76b0e2b317a9
yallmoong <victoria0404@naver.com>
2023-09-28 22:46:19 +0900

    경계면 검사 틀 완성

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -7,7 +7,7 @@ character = load_image('Pikachu.png')
 
 
 def handle_events():
-    global running, dir, action, move, frame, jump_x
+    global running, dir, action, frame, jump_x
 
     events = get_events()
     for event in events:
@@ -15,29 +15,32 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_a: #좌
-                if dir != 1:
-                    frame = 0
+                if dir != 1: frame = 0
                 dir, action = 1, 1
+
             elif event.key == SDLK_d: #우
-                if dir != 2:
-                    frame = 0
+                if dir != 2: frame = 0
                 dir, action = 2, 1
+
             elif event.key == SDLK_w: #상
-                if dir != 3:
-                    frame = 0
+                if dir != 3: frame = 0
                 dir, action = 3, 1
+
             elif event.key == SDLK_s: #하
-                if dir != 4:
-                    frame = 0
+                if dir != 4: frame = 0
                 dir, action = 4, 1
+
             elif event.key == SDLK_ESCAPE: #종료
                 running = False
+
             elif event.key == SDLK_SPACE: #점프
                 jump_x, frame = 0, 0
                 action = 2
+
             elif event.key == SDLK_e: #전기
                 jump_x, frame = 0, 0
                 action = 3
+
         elif event.type == SDL_KEYUP:
             if (dir == 1 and event.key == SDLK_a) or (dir == 2 and event.key == SDLK_d) or (dir == 3 and event.key == SDLK_w) or (dir == 4 and event.key == SDLK_s):
                 frame, action = 0, 0
@@ -46,16 +49,14 @@ def handle_events():
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-frame = 0
-dir = 1
-action = 0
+frame, action, jump_x, dir = 0, 0, 0, 1
 jump_width = [47, 46, 55, 42, 36, 33, 50, 49, 46, 40, 67, 37, 50, 51, 46, 48, 51, 65, 63, 61, 52]
-jump_x = 0
 
 while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
 
+    # 정지
     if action == 0:
         if dir % 2 == 1:
             character.clip_draw(frame * 51, 597 - 48, 51, 48, x, y, 51 * 2, 48 * 2)
@@ -63,6 +64,7 @@ while running:
             character.clip_composite_draw(frame * 51, 597 - 48, 51, 48, 0, 'h', x, y, 51 * 2, 48 * 2)
         frame = (frame + 1) % 8
 
+    # 뛰기
     elif action == 1:
         if dir == 1:
             x -= 10
@@ -73,12 +75,23 @@ while running:
         elif dir == 4:
             y -= 10
 
-        if dir % 2 == 1:
-            character.clip_draw(frame * 57, 597 - 145, 57, 47, x, y, 57 * 2, 47 * 2)
+        if x - 57 / 2 > 0 and x + 57 / 2 < TUK_WIDTH and y - 48 / 2 > 0 and y + 48 / 2 < TUK_HEIGHT:
+            if dir % 2 == 1:
+                character.clip_draw(frame * 57, 597 - 145, 57, 47, x, y, 57 * 2, 47 * 2)
+            else:
+                character.clip_composite_draw(frame * 57, 597 - 145, 57, 47, 0, 'h', x, y, 57 * 2, 47 * 2)
+            frame = (frame + 1) % 5
         else:
-            character.clip_composite_draw(frame * 57, 597 - 145, 57, 47, 0, 'h', x, y, 57 * 2, 47 * 2)
-        frame = (frame + 1) % 5
+            if x - 57 / 2 > 0:
+                pass
+            elif x + 57 / 2 < TUK_WIDTH:
+                pass
+            elif y - 48 / 2 > 0:
+                pass
+            elif y + 48 / 2 < TUK_HEIGHT:
+                pass
 
+    # 점프
     elif action == 2:
         if dir % 2 == 1:
             character.clip_draw(jump_x, 597 - 247, jump_width[frame], 100, x, y + 20, jump_width[frame] * 2, 100 * 2)
@@ -87,14 +100,17 @@ while running:
         jump_x += jump_width[frame]
         frame = (frame + 1) % 21
         if frame == 0:
-            jump_x = 0
+            action = 0
 
+    # 전기
     elif action == 3:
         if dir % 2 == 1:
             character.clip_draw(frame * 53, 597 - 295, 53, 45, x, y, 53 * 2, 45 * 2)
         else:
             character.clip_composite_draw(frame * 53, 597 - 295, 53, 45, 0, 'h', x, y, 53 * 2, 45 * 2)
         frame = (frame + 1) % 17
+        if frame == 0:
+            action = 0
         pass
 
     update_canvas()
